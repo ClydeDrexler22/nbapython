07cc83726648fdb596a1e3c324ee48881a27a4ca
Joshua Tanner <biolsurfer85@gmail.com>
2015-11-01 15:37:33 -0800

    Add basketball db

---
+++ db/Basketballdb.py
@@ -0,0 +1,28 @@
+import csv
+
+class Basketballdb(object):
+    """Retrieve basketball information"""
+    players = None;
+    teams = None;
+    def __init__(self):
+        self.dbloc = './db/players.csv'
+    def getPlayers(self):
+        players = []
+        if(not self.players):
+            with open('./db/players.csv') as playerscsv:
+                reader = csv.DictReader(playerscsv)
+                for idx,row in enumerate(reader):
+                    player = row
+                    players.append(player)
+            self.players = players
+        return self.players
+    def getTeams(self):
+        teams = []
+        if(not self.teams):
+            with open('./db/teams.csv') as teamscsv:
+                reader = csv.DictReader(teamscsv)
+                for idx,row in enumerate(reader):
+                    team = row
+                    teams.append(team)
+            self.teams = teams
+        return self.teams
